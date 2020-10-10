88f498ed6734c112b66140426fe3d3ca49ee5419
Colin Okay <cbeok@protonmail.com>
2020-10-10 10:39:06 -0500

    added some extensions

--- src/haxe/ds/Maybe.hx
+++ src/haxe/ds/Maybe.hx
@@ -2,72 +2,92 @@ package haxe.ds;
 
 @:using(haxe.ds.Maybe.MaybeUtil)
 enum Maybe<T> {
-    Just(t:T);
-    Nothing;
+  Just(t:T);
+  Nothing;
 }
 
 class MaybeUtil {
-
-    public static function isJust<T>(o:Maybe<T>): Bool {
-	return switch(o) {
-	    case Just(_): true;
-	    case Nothing: false;
-	};
+  
+  public static function isJust<T>(o:Maybe<T>): Bool {
+    return switch(o) {
+    case Just(_): true;
+    case Nothing: false;
+    };
+  }
+  
+  public static function isNothing<T>(o:Maybe<T>): Bool {
+    return !o.isJust();
+  }
+  
+  public static function map<T,U>( o:Maybe<T>, fn : (t:T) -> U) : Maybe<U> {
+    return switch(o) {
+    case Just(t): Just(fn(t));
+    case Nothing: Nothing;
+    };
+  }
+  
+  public static function thenDo<T>( o:Maybe<T>, fn: (t:T) -> Void) {
+    map(o,fn);
+  }
+  
+  public static function onJust<T>( o:Maybe<T>, fn: (t:T) -> Void):Maybe<T> {
+    map(o,fn);
+    return o;
+  }
+  
+  public static function onNothing<T>( o: Maybe<T>, fn: () -> Void):Maybe<T> {
+    switch(o) {
+    case Nothing: fn();
+    default: {}
     }
-    
-    public static function isNothing<T>(o:Maybe<T>): Bool {
-	return !o.isJust();
-    }
+    return o;
+  }
+  
+  public static function andThen<T,U>( o:Maybe<T>, fn : (t:T) -> Maybe<U>) : Maybe<U> {
+    return switch(o) {
+    case Just(t): fn(t);
+    case Nothing: Nothing;
+    };
+  }
+  
+  public static function is<T>( o : Maybe<T>, pred: (t:T) -> Bool) : Bool {
+    return switch(o) {
+    case Just(t): pred(t);
+    case Nothing: false;
+    };
+  }
+  
+  public static function when<T>( o : Maybe<T>, pred: (t:T) -> Bool) : Maybe<T> {
+    return if (is(o,pred)) o else Nothing;
+  }
+  
+  // allows chaining of several clauses, shortcircuting with Nothing on the first success.
+  public static function condDo<T,U>( o : Maybe<T>,
+                                      pred: (t:T) -> Bool,
+                                      then: (t:T) -> Void) : Maybe<T>
+  {
+    return if ( when(o, pred).onJust( then ).isJust() ) Nothing else o;
+  }
+  
+  public static function take<T>(o : Maybe<T>, ?defaultValue : Null<T> = null) : Null<T> {
+    return switch(o) {
+    case Nothing: defaultValue;
+    case Just(t): t;
+    };
+  }
+  
+  public static function maybe<T>(o:Null<T>):Maybe<T> {
+    if (o == null) return Nothing;
+    return Just(o);
+  }
 
-    public static function map<T,U>( o:Maybe<T>, fn : (t:T) -> U) : Maybe<U> {
-	return switch(o) {
-	    case Just(t): Just(fn(t));
-	    case Nothing: Nothing;
-	};
-    }
-
-    public static function thenDo<T>( o:Maybe<T>, fn: (t:T) -> Void) {
-	map(o,fn);
-    }
-
-    public static function onJust<T>( o:Maybe<T>, fn: (t:T) -> Void):Maybe<T> {
-	map(o,fn);
-	return o;
-    }
-
-    public static function onNothing<T>( o: Maybe<T>, fn: () -> Void):Maybe<T> {
-	switch(o) {
-	case Nothing: fn();
-	default: {}
-	}
-	return o;
-    }
-
-    public static function andThen<T,U>( o:Maybe<T>, fn : (t:T) -> Maybe<U>) : Maybe<U> {
-	return switch(o) {
-	    case Just(t): fn(t);
-	    case Nothing: Nothing;
-	};
-    }
-
-    public static function is<T>( o : Maybe<T>, pred: (t:T) -> Bool) : Bool {
-	return switch(o) {
-	    case Just(t): pred(t);
-	    case Nothing: false;
-	};
-    }
-
-
-    public static function when<T>( o : Maybe<T>, pred: (t:T) -> Bool) : Maybe<T> {
-	return if (is(o,pred)) o else Nothing;
-    }
-
-
-    public static function take<T>(o : Maybe<T>, ?defaultValue : Null<T> = null) : Null<T> {
-	return switch(o) {
-	    case Nothing: defaultValue;
-	    case Just(t): t;
-	};
-    }
-
+  public static function maybeMap<T,U>(ts: Array<T>, fn: (t:T) -> Maybe<U>): Maybe<Array<U>> {
+    var res: Array<U> = [];
+    for (t in ts) switch (fn(t)) {
+      case Nothing: return Nothing;
+      case Just(u): res.push(u);
+      }
+    return Just(res);
+  }
+  
 }
--- src/haxe/ds/Result.hx
+++ src/haxe/ds/Result.hx
@@ -2,59 +2,77 @@ package haxe.ds;
 
 @:using(Result.ResultExtensions)
 enum Result<E,T> {
-    Err(kind:E);
-    Ok(ok:T);
+  Err(kind:E);
+  Ok(ok:T);
 }
 
 class ResultExtensions
 {
+  
+  public static function isOk<E,T>(result: Result<E,T>): Bool {
+    return switch (result) {
+    case Ok(_): true;
+    case _: false;
+    };
+  }
+  
+  public static function isErr<E,T>(result: Result<E,T>): Bool {
+    return !isOk(result);
+  }
+  
+  public static function then<E,T,U>(result: Result<E,T>,
+                                     fn: (val:T) -> Result<E,U>
+                                     ): Result<E,U>
+  {
+    return switch (result) {
+    case Ok(val): fn(val);
+    case Err(e): Err(e);
+    };
+  }
 
-    public static function isOk<E,T>(result: Result<E,T>): Bool {
-	return switch (result) {
-	    case Ok(_): true;
-	    case _: false;
-	};
+  public static function onOk<E,T>(result: Result<E,T>,
+                                   fn: (val:T) -> Void
+                                   ): Result<E,T>
+  {
+    return then(result, val -> {fn(val); return result;});
+  }      
+  
+  public static function onError<E,T>(result: Result<E,T>,
+                                      fn: (err:E) -> Void
+                                      ): Result<E,T>
+  {
+    switch (result) {
+    case Err(e): fn(e);
+    case _: {}
     }
-
-    public static function isErr<E,T>(result: Result<E,T>): Bool {
-	return !isOk(result);
-    }
-
-    public static function then<E,T,U>(result: Result<E,T>,
-				       fn: (val:T) -> Result<E,U>
-				      ): Result<E,U>
-	{
+    return result;
+  }
+  
+  public static function map<E,T,U>(result: Result<E,T>,
+                                    fn: (val:T) -> U
+                                    ): Result<E,U>
+  {
 	    return switch (result) {
-		case Ok(val): fn(val);
-		case Err(e): Err(e);
+            case Ok(v): Ok(fn(v));
+            case Err(e): Err(e);
 	    };
-	}
+  }
+  
+  public static function resultMap<E,T,U>(ts: Array<T>,
+                                          fn: (t:T) -> Result<E,U>
+                                          ): Result<E,Array<U>> {
+    var res: Array<U> = [];
+    for (t in ts) switch(fn(t)) {
+      case Ok(u): res.push(u);
+      case Err(e): return Err(e);
+      }
+    return Ok(res);
+  }
 
-    public static function onOk<E,T>(result: Result<E,T>,
-				     fn: (val:T) -> Void
-				    ): Result<E,T>
-	{
-	    return then(result, val -> {fn(val); return result;});
-	}      
-
-    public static function onError<E,T>(result: Result<E,T>,
-					fn: (err:E) -> Void
-				       ): Result<E,T>
-	{
-	    switch (result) {
-	    case Err(e): fn(e);
-	    case _: {}
-	    }
-	    return result;
-	}
-
-    public static function map<E,T,U>(result: Result<E,T>,
-				      fn: (val:T) -> U
-				     ): Result<E,U>
-	{
-	    return switch (result) {
-		case Ok(v): Ok(fn(v));
-		case Err(e): Err(e);
-	    };
-	}
+  public static function fromMaybe<E,T>(m:Maybe<T>, e: E) : Result<E,T> {
+    return switch (m) {
+    case Just(t): Ok(t);
+    case Nothing: Err(e);
+    };
+  }
 }
